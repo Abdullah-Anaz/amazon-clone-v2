d3d5fe43bc8fade8429a656e6d8f239b862c9cfc
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-05-01 06:26:36 +0530

    cleanup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Y2xthRVXtMkH1dWd09V4vIvQwFs_te4",
+  authDomain: "clone-cd4d8.firebaseapp.com",
+  projectId: "clone-cd4d8",
+  storageBucket: "clone-cd4d8.appspot.com",
+  messagingSenderId: "473706427295",
+  appId: "1:473706427295:web:10ef940d347c6022fe43aa",
+  measurementId: "G-Y43LMR3H0X",
+};
