133d03dd58c964c778533be02541f9dba77fd24d
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-05-16 02:37:51 +0530

    Fix: Routes Error

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Home />} />
         </Routes>
       </div>
     </Router>
